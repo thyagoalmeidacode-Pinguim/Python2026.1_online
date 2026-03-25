790e24b8de25931152d691f1820bb71c0f261547
thyagoalmeidacode-Pinguim <thyagoalmeidacode@gmail.com>
2026-03-25 15:30:59 -0300

    Aula_

---
+++ Aula_/replace_isdigit.py
@@ -0,0 +1,11 @@
+nome_cadastro = "Thyago Assis de Almeida"
+print(nome_cadastro)
+
+print(nome_cadastro.replace(" ",""))
+
+valor = "100.0.0"
+print(valor.replace(".","",1))
+
+#Função isdigit()
+idade = "Senac"
+print(idade.isdigit())
